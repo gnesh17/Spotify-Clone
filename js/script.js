2820f299751e814ed1cb84a19e80b65d8a034d12
gnesh17 <ganeshsonone549@gmail.com>
2025-03-27 02:43:34 +0530

    commit 1

--- js/script.js
+++ js/script.js
@@ -21,7 +21,9 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`/${folder}/`)
+    // let a = await fetch(`/${folder}/`)
+    let a = await fetch(`https://spotify-by-gnesh.netlify.app/${folder}/`)
+
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -101,7 +103,10 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`/songs/`)
+    // let a = await fetch(`/songs/`)
+    let a = await fetch(`https://spotify-by-gnesh.netlify.app/songs/`)
+
+
     let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
@@ -113,7 +118,10 @@ async function displayAlbums() {
         if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
-            let a = await fetch(`/songs/${folder}/info.json`)
+
+            // let a = await fetch(`/songs/${folder}/info.json`)
+            let a = await fetch(`https://spotify-by-gnesh.netlify.app/songs/${folder}/info.json`)
+
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
             <div class="play">
