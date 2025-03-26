b63adb9b3c18a89ddbc476acf3bf67f5ac8440ef
gnesh17 <ganeshsonone549@gmail.com>
2025-03-27 02:45:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,8 +21,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currFolder = folder;
-    // let a = await fetch(`/${folder}/`)
-    let a = await fetch(`https://spotify-by-gnesh.netlify.app/${folder}/`)
+    let a = await fetch(`/${folder}/`)
 
     let response = await a.text();
     let div = document.createElement("div")
@@ -103,8 +102,7 @@ const playMusic = (track, pause = false) => {
 
 async function displayAlbums() {
     console.log("displaying albums")
-    // let a = await fetch(`/songs/`)
-    let a = await fetch(`https://spotify-by-gnesh.netlify.app/songs/`)
+    let a = await fetch(`/songs/`)
 
 
     let response = await a.text();
@@ -119,8 +117,7 @@ async function displayAlbums() {
             let folder = e.href.split("/").slice(-2)[0]
             // Get the metadata of the folder
 
-            // let a = await fetch(`/songs/${folder}/info.json`)
-            let a = await fetch(`https://spotify-by-gnesh.netlify.app/songs/${folder}/info.json`)
+            let a = await fetch(`/songs/${folder}/info.json`)
 
             let response = await a.json();
             cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
